017a311f115c99dfbbe42bc60c33e1e3e06f1150
anhvu2501 <vu25ck28@gmail.com>
2022-06-01 15:39:59 +0700

    basic gradient torch using simple Linear Regression

--- Pytorch/gradients_torch.py
+++ Pytorch/gradients_torch.py
@@ -7,28 +7,43 @@
 import torch
 import torch.nn as nn
 
-X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
-Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
+Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
+X_test = torch.tensor([5], dtype=torch.float32)
 
-w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+n_samples, n_features = X.shape
+
+input_size = n_features
+output_size = n_features
 
 
-# model prediction
-def forward(x):
-    return w * x
+# model = nn.Linear(input_size, output_size)
 
-print(f'Predition before training: f(5) = {forward(5):.3f}')
+class LinearRegression(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LinearRegression, self).__init__()
+        # define layers
+        self.lin = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        x = self.lin(x)
+        return x
+
+
+model = LinearRegression(input_size, output_size)
+
+print(f'Predition before training: f(5) = {model(X_test).item():.3f}')
 
 # Training
 learning_rate = 0.01
 n_iters = 100
 
 loss = nn.MSELoss()
-optimizer = torch.optim.SGD([w], lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 for epoch in range(n_iters):
     # Prediction = forward pass
-    y_pred = forward(X)
+    y_pred = model(X)
 
     # Loss
     l = loss(Y, y_pred)
@@ -43,6 +58,7 @@ for epoch in range(n_iters):
     optimizer.zero_grad()
 
     if epoch % 10 == 0:
-        print(f'epoch {epoch + 1}: w = {w:.3f}, loss = {l:.8f}')
+        [w, b] = model.parameters()
+        print(f'epoch {epoch + 1}: w = {w[0][0].item():.3f}, loss = {l:.8f}')
 
-print(f'Prediction after training: f(5) = {forward(5):.3f}')
+print(f'Prediction after training: f(5) = {model(X_test).item():.3f}')
